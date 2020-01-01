dbb69a5921020a899cf4478374f382eec4214b4e
Luke K Schubert <lkschubert8@gmail.com>
2020-01-01 11:40:36 -0500

    have single char parsing tested and working

--- src/lege/core.clj
+++ src/lege/core.clj
@@ -1,29 +1,26 @@
-(ns lege.core
-  (:require 
-   [clojure.spec.alpha :as spec]))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns lege.core)
 
 ;;For starters I want to be able to parse a single specified char, multiple chars
 ;;wildcard characters, sequences and between
+
+(defn- build-success
+  [sequence result]
+  {:sequence sequence
+   :result result})
+
+(defn- build-error
+  [message]
+  {:error message})
+
+(defn- is-error
+  [parser-result]
+  (contains? parser-result :error))
 
 (defn parse-char 
   ([character]
   (fn [sequence] 
     (let [current-value (first sequence)]
     (if (= current-value character)
-      [(rest sequence) character]
-      [(rest sequence) {:error :error}] ;; TODO figure out error handling/threading through parsers 
-      ))
-    )))
-
-(defn parse-any
-  [parsers]
-  (fn [sequence] (throw (Exception. "Not implemented"))))
-
-(defn parse-many
-  [parser]
-  (fn [sequence] (throw (Exception. "Not implemented."))))
+      (build-success (rest sequence) (first sequence))
+      (build-error (str "Expecting '" character "' found '" (first sequence) "'")) ;; TODO figure out error handling/threading through parsers 
+      )))))
--- test/lege/core_test.clj
+++ test/lege/core_test.clj
@@ -5,16 +5,8 @@
 
 (deftest test-basic-char-parser
   (testing "character parser"
-    (is (= ((lege/parse-char \x) [\x]) [[] \x])))
+    (is (= ((lege/parse-char \x) [\x]) {:sequence [] :result \x})))
   (testing "character parser expected failure"
-    (is (= ((lege/parse-char \y) [\x]) [[] {:error :error}]))))
+    (is (= ((lege/parse-char \y) [\x])  {:error "Expecting 'y' found 'x'"}))))
 
-(deftest test-any-combinator
-  (testing "Checking if the any combinator works as expected"
-    (is
-     (= [[] \y]
-        ((lege/parse-any
-         [(lege/parse-char \x)
-         (lege/parse-char \y)]) [\y]))))
-  #_(testing "Checking the any combinator fails when expected")
-  #_(testing "Checking the any combinator operates with the expected precedence"))
+
