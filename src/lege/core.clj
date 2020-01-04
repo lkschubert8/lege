7c87b96e7b3c33b3ceef8703e59d91a1c33b379c
Luke K Schubert <lkschubert8@gmail.com>
2020-01-04 17:01:59 -0500

    All of core spec'd/instrumented and tests still passing

--- src/lege/core.clj
+++ src/lege/core.clj
@@ -33,8 +33,8 @@
     (let [current-value (first sequence)]
       (if (= current-value val)
         (build-success (rest sequence) (first sequence))
-        (build-error (str "Expecting '" val "' found '" (first sequence) "'")) ;; TODO figure out error handling/threading through parsers 
-        ))))
+        (build-error (str "Expecting '" val "' found '" (first sequence) "'")))))) ;; TODO figure out error handling/threading through parsers 
+        
 
 (defn-spec and-then :lege/parser
   [parser-a :lege/parser parser-b :lege/parser]
@@ -55,47 +55,47 @@
         (parser-b sequence)
         result-a))))
 
-(defn choice
-  [parsers]
+(defn-spec choice :lege/parser
+  [parsers (s/* :lege/parser)]
   (fn [sequence]
     ((reduce or-else parsers) sequence)))
 
-(defn any-of
-  [vals]
+(defn-spec any-of :lege/parser 
+  [vals :lege/sequence]
   (choice (map parse-val vals)))
 
-(defn map-parser
-  [map-fn parser]
+(defn-spec map-parser :lege/parser
+  [map-fn fn? parser :lege/parser]
   (fn [sequence]
     (let [result (parser sequence)]
       (if (is-error result)
         result
         (update result :lege/result map-fn)))))
 
-(defn return-parser
-  [input]
+(defn-spec return-parser :lege/parser
+  [input any?]
   (fn [sequence] (build-success sequence input)))
 
-(defn apply-parser ; TODO spend more time grokking this
-  [fp xp]
+(defn-spec apply-parser :lege/parser
+  [fp fn? xp :lege/parser]
   (map-parser #((first %) (second %)) (and-then fp xp)))
 
 
 
-(defn- lift2
+(defn- lift2 
   [f]
   (fn [xp yp]
     (apply-parser (apply-parser (return-parser f) xp) yp)))
 
-(defn sequence-parser
-  [parsers]
+(defn-spec sequence-parser :lege/parser
+  [parsers (s/* :lege/parser)]
   (let [cons-parser (lift2 #(partial cons %))]
     (if (= 0 (count parsers))
       (return-parser [])
       (cons-parser (first parsers) (sequence-parser (rest parsers))))))
 
-(defn parse-string ;; TODO This still assume seq of chars 
-  [string]
+(defn-spec parse-string :lege/parser ;; TODO This still assume seq of chars 
+  [string string?]
   (fn
     [sequence]
     ((map-parser #(apply str %) (sequence-parser (map parse-val (seq string)))) sequence)))
@@ -111,12 +111,12 @@
                rest :lege/sequence} ((parse-zero-or-more parser) (:lege/sequence result))]
           (build-success rest (cons (:lege/result result) vals)))))))
 
-(defn parse-many
-  [parser]
+(defn-spec parse-many :lege/parser
+  [parser :lege/parser]
   (parse-zero-or-more parser))
 
-(defn parse-many-1
-  [parser]
+(defn-spec parse-many-1 :lege/parser
+  [parser :lege/parser]
   (fn
     [sequence]
     (let [result (parser sequence)]
@@ -126,30 +126,30 @@
                rest :lege/sequence} ((parse-zero-or-more parser) (:lege/sequence result))]
           (build-success rest (cons (:lege/result result) vals)))))))
 
-(defn parse-opt
-  [parser]
+(defn-spec parse-opt :lege/parser
+  [parser :lege/parser]
   (or-else parser
            (return-parser nil)))
 
-(defn and-then-ignore-left
-  [parser-a parser-b]
+(defn-spec and-then-ignore-left :lege/parser
+  [parser-a :lege/parser parser-b :lege/parser]
   (map-parser second (and-then parser-a parser-b)))
 
-(defn and-then-ignore-right
-  [parser-a parser-b]
+(defn-spec and-then-ignore-right :lege/parser
+  [parser-a :lege/parser parser-b :lege/parser]
   (map-parser first (and-then parser-a parser-b)))
 
-(defn parse-between
-  [parser-a parser-b parser-c]
+(defn-spec parse-between :lege/parser
+  [parser-a :lege/parser parser-b :lege/parser parser-c :lege/parser]
   (and-then-ignore-right (and-then-ignore-left parser-a parser-b) parser-c))
 
-(defn parse-sep-by-1
-  [parser separator-parser]
+(defn-spec parse-sep-by-1 :lege/parser
+  [parser :lege/parser separator-parser :lege/parser]
   (let [separator-then-parser (and-then-ignore-left separator-parser parser)]
     (map-parser #(cons (first %) (second %)) (and-then parser (parse-many separator-then-parser)))))
 
-(defn parse-sep-by
-  [parser separator-parser]
+(defn-spec parse-sep-by :lege/parser
+  [parser :lege/parser separator-parser :lege/parser]
   (or-else (parse-sep-by-1 parser separator-parser) (return-parser [])))
 
 (defn parse-bind
